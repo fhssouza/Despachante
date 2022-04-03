59488f9984f377e158c408f6458575c2b6ef8ff2
fhssouza <fhssouza@gmail.com>
2022-04-03 14:02:51 -0300

    Criando o projeto backend

---
+++ backend/src/main/java/com/projetos/despachante/DespachanteApplication.java
@@ -0,0 +1,13 @@
+package com.projetos.despachante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespachanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespachanteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projetos/despachante/DespachanteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos.despachante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DespachanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
