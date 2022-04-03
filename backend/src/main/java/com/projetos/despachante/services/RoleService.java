c4bb353aa4de67df4f8bfd932dbaaf9f50d74478
fhssouza <fhssouza@gmail.com>
2022-04-03 18:27:23 -0300

    Database seeding

--- backend/src/main/java/com/projetos/despachante/services/RoleService.java
+++ backend/src/main/java/com/projetos/despachante/services/RoleService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.projetos.despachante.entities.Role;
 import com.projetos.despachante.repositories.RoleRepository;
@@ -14,6 +15,7 @@ public class RoleService {
 	@Autowired
 	private RoleRepository repository;
 
+	@Transactional(readOnly = true)
 	public List<Role> findAll(){
 		return repository.findAll();
 	}
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_role(authority) VALUES ('Administrador');
+INSERT INTO tb_role(authority) VALUES ('Supervisor');
+INSERT INTO tb_role(authority) VALUES ('Usuario');
