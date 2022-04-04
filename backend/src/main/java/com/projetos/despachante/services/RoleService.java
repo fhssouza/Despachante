c53f8f260db6c8aec03af8ebddd5fb220cc3801f
fhssouza <fhssouza@gmail.com>
2022-04-03 22:25:32 -0300

    Delete Role

--- backend/src/main/java/com/projetos/despachante/services/RoleService.java
+++ backend/src/main/java/com/projetos/despachante/services/RoleService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.projetos.despachante.dto.RoleDTO;
 import com.projetos.despachante.entities.Role;
 import com.projetos.despachante.repositories.RoleRepository;
+import com.projetos.despachante.services.exceptions.DatabaseException;
 import com.projetos.despachante.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -56,5 +59,15 @@ public class RoleService {
 			throw new ResourceNotFoundException("Id not found" + id);
 		}
 
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
 	}
 }
