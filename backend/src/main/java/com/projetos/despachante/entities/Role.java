fd92e43a22263c5b13f96c75a27fdca13715e15e
fhssouza <fhssouza@gmail.com>
2022-04-03 22:48:53 -0300

    Auditing data

--- backend/src/main/java/com/projetos/despachante/entities/Role.java
+++ backend/src/main/java/com/projetos/despachante/entities/Role.java
@@ -1,23 +1,33 @@
 package com.projetos.despachante.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_role")
-public class Role implements Serializable{
+public class Role implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String authority;
+
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
 
 	public Role() {
 
@@ -42,6 +52,24 @@ public class Role implements Serializable{
 
 	public void setAuthority(String authority) {
 		this.authority = authority;
+	}
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
 	}
 
 	@Override
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_role(authority) VALUES ('Administrador');
-INSERT INTO tb_role(authority) VALUES ('Supervisor');
-INSERT INTO tb_role(authority) VALUES ('Usuario');
+INSERT INTO tb_role(authority, created_At) VALUES ('Administrador', NOW());
+INSERT INTO tb_role(authority, created_At) VALUES ('Supervisor', NOW());
+INSERT INTO tb_role(authority, created_At) VALUES ('Usuario', NOW());
