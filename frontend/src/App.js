2c11befc5a1ccdd04978c07fa33e43b8f8ca884d
AlvesBelem <marcelocartoes.pa@gmail.com>
2022-04-04 22:59:07 -0300

    Background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
-import {Login} from './pages/Login'
-import {Register} from './pages/Register'
-import {Home} from './pages/Home'
+import { Login } from './pages/Login'
+import { Register } from './pages/Register'
+import { Home } from './pages/Home'
 
 function App() {
   return (
