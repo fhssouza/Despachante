e1edbb81faee97b1714dd3c2e1c32a853dadf849
fhssouza <fhssouza@gmail.com>
2022-04-22 18:34:32 -0300

    Cors Configuration

--- backend/src/main/java/com/projetos/despachante/dto/UserDTO.java
+++ backend/src/main/java/com/projetos/despachante/dto/UserDTO.java
@@ -39,7 +39,7 @@ public class UserDTO implements Serializable {
 	public UserDTO(User entity) {
 		id = entity.getId();
 		firstName = entity.getFirstName();
-		lastName = entity.getEmail();
+		lastName = entity.getLastName();
 		email = entity.getEmail();
 		entity.getRoles().forEach(role -> this.roles.add(new RoleDTO(role)));
 	}
